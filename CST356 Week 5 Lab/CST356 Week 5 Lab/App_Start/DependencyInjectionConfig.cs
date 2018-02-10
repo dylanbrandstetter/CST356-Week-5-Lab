ab2fe36491c2398e9430d4cf0b9f2ec109a09a7e
dylanbrandstetter <35245920+dylanbrandstetter@users.noreply.github.com>
2018-02-10 14:53:23 -0800

    Exercise 2

---
+++ CST356 Week 5 Lab/CST356 Week 5 Lab/App_Start/DependencyInjectionConfig.cs
@@ -0,0 +1,24 @@
+﻿using CST356_Week_5_Lab.Data;
+using CST356_Week_5_Lab.Repositories;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CST356_Week_5_Lab.App_Start
+{
+    public static class DependencyInjectionConfig
+    {
+        public static void Register()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            // Register your types, for instance:
+            container.Register<IAppRepository, AppRepository>(Lifestyle.Scoped);
+            container.Register<MyDbContext, MyDbContext>(Lifestyle.Scoped);
+        }
+}
